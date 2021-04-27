728510ccc8af4d30b8bb0fb59b5f33be5475cb37
Marina Dariy <dariy.mvs@gmail.com>
2021-04-27 12:37:32 +0300

    refresh user.test

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -1,6 +1,10 @@
 import getLevel from '../app';
 import fetchData from '../http';
 
+test('fetchData', () => {
+  expect(fetchData()).toBe(undefined);
+});
+
 jest.mock('../http');
 
 beforeEach(() => {
@@ -12,4 +16,13 @@ test('getLevel', () => {
   getLevel(1);
   expect(fetchData).toBeCalledTimes(1);
   expect(fetchData).toHaveBeenCalledWith('https://server/user/1');
+});
+
+test('getLevel', () => {
+  const responseObj = {
+    status: 'ok',
+    level: 5,
+  };
+  fetchData.mockReturnValue(responseObj);
+  expect(getLevel(1)).toEqual(`Ваш текущий уровень: ${5}`);
 });
