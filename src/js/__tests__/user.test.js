22c69089981e78f910082f54007a46ade0013f37
Marina Dariy <dariy.mvs@gmail.com>
2021-04-27 09:38:00 +0300

    task 3

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -1,6 +1,5 @@
-import { loadUser } from '../user';
-import { httpGet } from '../http';
-import sortHeroes from '../app';
+import getLevel from '../app';
+import fetchData from '../http';
 
 jest.mock('../http');
 
@@ -8,25 +7,9 @@ beforeEach(() => {
   jest.resetAllMocks();
 });
 
-test('should call loadUser once', () => {
-  httpGet.mockReturnValue(JSON.stringify({}));
-
-  const response = loadUser(1);
-  expect(response).toEqual({});
-  expect(httpGet).toBeCalledWith('http://server:8080/users/1');
-});
-
-test('sortHeroes', () => {
-  const result = sortHeroes([
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'лучник', health: 80 },
-  ]);
-  expect(result).toEqual([
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ]);
+test('getLevel', () => {
+  fetchData.mockReturnValue('{}');
+  getLevel(1);
+  expect(fetchData).toBeCalledTimes(1);
+  expect(fetchData).toHaveBeenCalledWith('https://server/user/1');
 });
